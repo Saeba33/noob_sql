37c431df1b407e7757d6bc3a5f11ba179bcd1b34
Charles Proust <konoha33@hotmail.fr>
2025-11-15 16:04:59 +0100

    edit favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata = {
 	title: "NoobSQL",
 	description: "Apprenez SQL avec le système de ceintures",
+	icons: {
+		icon: "/favicon.ico",
+	},
 };
 
 export default function RootLayout({ children }) {
