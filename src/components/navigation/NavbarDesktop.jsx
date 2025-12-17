91293c5620d74da9d88995089b71cb1e886f5df6
Charles Proust <konoha33@hotmail.fr>
2025-12-17 17:14:54 +0100

    edit navbar

--- src/components/navigation/NavbarDesktop.jsx
+++ src/components/navigation/NavbarDesktop.jsx
@@ -8,13 +8,6 @@ import Link from "next/link";
 export default function NavbarDesktop() {
 	const { isActive } = useNavigation();
 
-	// Helper function
-	const getDesktopLinkStyle = (active) => ({
-		className: `bg-transparent rounded-lg transition-all duration-200 px-2 pt-[7px] pb-[7px] flex items-center gap-2 text-sm ${
-			active ? "font-semibold text-gray-900" : "font-light text-gray-600 hover:text-gray-900"
-		} whitespace-nowrap`,
-	});
-
 	return (
 		<nav className="flex items-center gap-4 " aria-label="Navigation principale">
 			{PAGES_CONFIG.filter((item) => item.href !== "/practice").map(
@@ -27,10 +20,12 @@ export default function NavbarDesktop() {
 							key={`${beltKey}-${index}`}
 							href={item.href}
 							aria-current={active ? "page" : undefined}
-							{...getDesktopLinkStyle(active)}
+							className={`bg-transparent rounded-lg transition-all duration-200 px-2 pt-[7px] pb-[7px] flex items-center gap-2 text-sm ${
+								active ? "font-semibold text-gray-900" : "font-light text-gray-600 hover:text-gray-900"
+							} whitespace-nowrap`}
 						>
-							<BeltIcon belt={beltKey} size={32} />
-							<span className="relative tracking-wide pb-1">
+							<BeltIcon belt={beltKey} size={32} className="pt-1.5"/>
+							<span className="relative tracking-wide">
 								{/* Invisible text with semibold to reserve space */}
 								<span className="invisible font-semibold" aria-hidden="true">
 									{item.title}
